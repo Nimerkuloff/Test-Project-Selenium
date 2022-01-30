dfa473dfd21b012ae7f45f50bdfb4b95f9d672c3
Nimerkuloff <Nimerkuloff@users.noreply.github.com>
2022-01-30 12:12:54 +0700

    increased sleep timer

--- test_items.py
+++ test_items.py
@@ -9,9 +9,9 @@ def test_add_book_button_presents(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
 
-    time.sleep(5)
+    time.sleep(30)
 
-    element = WebDriverWait(browser, 5).until(
+    element = WebDriverWait(browser, 30).until(
         EC.visibility_of_element_located((By.CSS_SELECTOR, '[class="btn-add-to-basket"]'))
     )
 
