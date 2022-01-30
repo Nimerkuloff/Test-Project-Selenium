56ef91e9492043d9d5a228a260afa929c13eb586
Nimerkuloff <Nimerkuloff@users.noreply.github.com>
2022-01-30 12:12:25 +0700

    changed fixture to comply to DRY

--- conftest.py
+++ conftest.py
@@ -11,27 +11,13 @@ def pytest_addoption(parser):
 def browser(request):
 
     user_language = request.config.getoption("language")
+    
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
 
-    if user_language == "en":
-        print("\nstart chrome browser for test in en..")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': 'en'})
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "es":
-        print("\nstart chrome browser for test in es..")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': 'es'})
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "fr":
-        print("\nstart chrome browser for test in fr..")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': 'fr'})
-        browser = webdriver.Chrome(options=options)
-    else:
-        print("\nstart chrome browser for test in fr..")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': 'fr'})
-        browser = webdriver.Chrome(options=options)
+    print(f"\nstart chrome browser for test in {user_language")
+  
 
     yield browser
     print("\nquit browser..")
