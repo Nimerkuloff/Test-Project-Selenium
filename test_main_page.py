5b0ef73ec9577a6bbf5c6603e7ba9d23ac0d368a
Nimerkuloff <Nimerkuloff@users.noreply.github.com>
2022-02-02 00:26:28 +0700

    Update test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,14 @@
-import time
-
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
 
-def test_add_book_button_presents(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(link)
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-    time.sleep(4)
-
-    element = browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')
-    assert len(element) != 0
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
