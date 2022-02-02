e1e3052f066ee97c2b9b06ab2ac6a006528c9d8c
Nimerkuloff <Nimerkuloff@users.noreply.github.com>
2022-02-02 23:13:53 +0700

    refactor LOGIN_LINK_LOCATOR

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,8 +4,8 @@ from selenium.webdriver.common.by import By
 
 class MainPage(BasePage): 
     def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK_LOCATOR), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.login_link_locator), "Login link is not presented"
 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK_LOCATOR)
+        login_link = self.browser.find_element(*MainPageLocators.login_link_locator)
         login_link.click()
