e0ac6b037e96aad1ff57af4dc953eccf5b3f3a83
Nimerkuloff <Nimerkuloff@users.noreply.github.com>
2022-02-02 23:10:03 +0700

    Add login page locators for its forms

--- pages/locators.py
+++ pages/locators.py
@@ -2,4 +2,8 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
-    LOGIN_LINK_LOCATOR = (By.CSS_SELECTOR, "#login_link")
+    login_link_locator = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    login_form_locator = (By.CSS_SELECTOR, "form#login_form")
+    reg_form_locator = (By.CSS_SELECTOR, "form#register_form")
