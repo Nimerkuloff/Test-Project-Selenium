820abcfeb8a28a2ca4a48c2682ba17d6d8d88d92
Nimerkuloff <Nimerkuloff@users.noreply.github.com>
2022-02-04 01:18:24 +0700

    Create test_product_page.py

---
+++ test_product_page.py
@@ -0,0 +1,8 @@
+from .pages.product_page import ProductPage
+
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/hacking-exposed-wireless_208/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket_AND_should_add_to_basket()
